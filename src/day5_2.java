5ed5ddcb3731de62d8d67734f9932e2ac6110260
Felix Gruber <felix.grub@gmail.com>
2015-12-06 05:14:35 +0100

    updated day5_1 (now using regex), added solution for day5_2

--- src/day5_2.java
+++ src/day5_2.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class day5_1 {
+public class day5_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("day5/input5.txt"));
         String line;
@@ -13,20 +13,12 @@ public class day5_1 {
         Matcher m;
 
         while ((line = br.readLine()) != null) {
-            // unallowed substrings
-            if (line.contains("ab") || line.contains("cd") || line.contains("pq") || line.contains("xy")) {
-                continue;
-            }
-
-            // vowels
-            p = Pattern.compile("[aeiou].*[aeiou].*[aeiou]");
+            p = Pattern.compile("(..).*\\1");
             m = p.matcher(line);
             if (!m.find()) {
                 continue;
             }
-
-            // double chars
-            p = Pattern.compile("(.)\\1");
+            p = Pattern.compile("(.).\\1");
             m = p.matcher(line);
             if (m.find()) {
                 num++;
