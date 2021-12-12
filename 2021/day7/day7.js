27f0e1675c9536bcb54cb41d7540b6570d2f1352
grubFX <2887162+grubFX@users.noreply.github.com>
2021-12-12 14:08:37 +0100

    ADD day7 part2

--- 2021/day7/day7.js
+++ 2021/day7/day7.js
@@ -2,19 +2,35 @@ import { readFileSync } from 'fs';
 const input = readFileSync('day7.txt', 'utf8')
 const lines = input.split(/\r?\n/)
 const numbers = lines[0].split(',').map(Number);
-
-let min = Math.min(...numbers);
-let max = Math.max(...numbers);
+const min = Math.min(...numbers), max = Math.max(...numbers);
 console.log('min: ' + min + ' max: ' + max);
+const array = [...Array(max - min + 1).keys()].map(i => i + min);
 
-let array = [...Array(max - min + 1).keys()].map(i => i + min);
-let sums = [];
-array.forEach(target => {
-    let sum = 0;
-    numbers.forEach(num => {
-        sum += Math.abs(num - target);
+function part1() {
+    let sums = [];
+    array.forEach(target => {
+        let sum = 0;
+        numbers.forEach(num => {
+            sum += Math.abs(num - target);
+        });
+        sums.push(sum);
     });
-    sums.push(sum);
-});
-let minSum = Math.min(...sums);
-console.log('minSum: ' + minSum + ' index:' + sums.indexOf(minSum));
+    let minSum = Math.min(...sums);
+    console.log('minSum: ' + minSum + ' target:' + (min + sums.indexOf(minSum)));
+}
+
+function part2() {
+    let sums = [];
+    array.forEach(target => {
+        let sum = 0;
+        numbers.forEach(num => {
+            sum += ([...Array(Math.abs(num - target) + 1).keys()]).reduce((a, b) => a + b);
+        });
+        sums.push(sum);
+    });
+    let minSum = Math.min(...sums);
+    console.log('minSum: ' + minSum + ' target:' + (min + sums.indexOf(minSum)));
+}
+
+part1();
+part2();
