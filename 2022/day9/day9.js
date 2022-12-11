7590aa7caaf2e54fa261eaf860eca2080febe64f
grubFX <2887162+grubFX@users.noreply.github.com>
2022-12-11 03:07:38 +0100

    FIX correct solution for day9

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -3,38 +3,34 @@ import Point from '@studiomoniker/point';
 const input = readFileSync('day9.txt', 'utf8');
 const lines = input.split(/\r?\n/)
 let parts = [], head = new Point(0, 0), tail = new Point(0, 0)
-const headMap = new Map(), tailMap = new Map()
+const headMap = new Map([["0/0", 1]]), tailMap = new Map([["0/0", 1]])
 
 lines.forEach(line => {
     parts = line.split(" ")
     let val = Number(parts[1])
-    switch (parts[0]) {
-        case "R":
-            head.x += val
-            break
-        case "L":
-            head.x -= val
-            break
-        case "U":
-            head.y += val
-            break
-        case "D":
-            head.y -= val
-            break
+    for (let i = 0; i < val; i++) {
+        switch (parts[0]) {
+            case "R": head.x++; break
+            case "L": head.x--; break
+            case "U": head.y++; break
+            case "D": head.y--; break
+        }
+        writeToMap(head, headMap)
+        calcTail()
     }
-    writeToMap(head, headMap)
-    calcTail()
 });
 
 function calcTail() {
-    if (head.getDistance(tail) > 1) {
+    let dist = head.getDistance(tail)
+    if (~~dist > 1) { // consider sqrt(2) as 1
         if (head.x > tail.x) tail.x++
         else if (head.x < tail.x) tail.x--
-        
+
         if (head.y > tail.y) tail.y++
         else if (head.y < tail.y) tail.y--
+
+        writeToMap(tail, tailMap)
     }
-    writeToMap(tail, tailMap)
 }
 
 function writeToMap(point, map) {
